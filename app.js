68efd7093eb7139ee58cfb820fb8367d9dc353f3
Lauret Marc-Olivier <Marco@localhost.localdomain>
2019-07-23 14:22:13 +0400

    Ver 0.1.2 - the application is now in real time

--- app.js
+++ app.js
@@ -1,61 +1,51 @@
-var express = require('express');
-var session = require('cookie-session');
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var express     = require('express');
+var http        = require('http');
+var ent         = require('ent');
 
 var app = express();
+var server      = http.createServer(app);
 
-app.use(session({secret: 'itsasecret'}))
-.use(express.static('public'))
+var io    = require('socket.io').listen(server);
 
-var create_if_empty = function(req, res, next){
-    if (typeof(req.session.match_list) == 'undefined') {
-        req.session.match_list = [];
-    }
-    next();
-}
+var match_list    = [];
+var index;           
 
-var show_list = function(req, res) { 
-    res.render('bf_manager.ejs', {match_list: req.session.match_list});
-}
+app.use (express.static('views'))
 
-var add_item = function(req, res) {
-    if (req.body.new_match != '') {
-        req.session.match_list.push({
-            content : req.body.new_match,
-            state : false
-        });
-    }
-    res.redirect('/bf_manager');
-}
-
-var rm_item = function(req, res) {
-    if (req.params.id != '') {
-        req.session.match_list.splice(req.params.id, 1);
-    }
-    res.redirect('/bf_manager');
-}
-
-var chk_state = function(req, res) {
-    if (req.params.id != '') {
-        let id = req.session.match_list.splice(req.params.id, 1);
-        if (id[0].state === false)
-            id[0].state = true;
-        else
-            id[0].state = false;
-        req.session.match_list.push(id[0]);
-    }
-    res.redirect('/bf_manager/');
-}
-
-app.use(create_if_empty);
-app.get('/bf_manager', show_list);
-app.post('/bf_manager/add/', urlencodedParser, add_item);
-app.get('/bf_manager/rm/:id', rm_item);
-app.get('/bf_manager/state/:id', chk_state);
-
-app.use(function(req, res, next){
-    res.redirect('/bf_manager');
+.get('/BF_manager', function(req, res)
+{
+    res.render('BF_manager.ejs');
 })
 
-.listen(8080);
+.use(function(req, res, next)
+{
+    res.redirect('/BF_manager');
+});
+
+io.sockets.on('connection', function(socket){
+    socket.emit('sync_board', match_list);
+    socket.on('add_match', function(match){
+        match = ent.encode(match);
+        match_list.push({content: match, state: false});
+        index = match_list.length -1;
+        socket.broadcast.emit('add_match', {match:match, index:index});
+        io.sockets.emit('sync_board', match_list);
+    });
+
+    socket.on('rm_match', function(index){
+        match_list.splice(index, 1);
+        io.sockets.emit('sync_board', match_list);
+    });
+
+    socket.on('new_state', function(index){
+        var name = match_list[index].content;
+        name = ent.encode(name);
+        if (match_list[index].state === true)
+            match_list[index].state = false;
+        else
+            match_list[index].state = true;
+        io.sockets.emit('sync_board', match_list);
+    });
+});
+
+server.listen(8080);
